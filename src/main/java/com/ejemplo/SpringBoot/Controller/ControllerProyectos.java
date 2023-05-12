34cc461e4331b45abc68e07a0ed9c15325a444d2
Natalia Juárez <juareznatalia94@gmail.com>
2023-05-12 20:24:09 -0300

    Cambios en las clases y conexión con firebase

--- src/main/java/com/ejemplo/SpringBoot/Controller/ControllerProyectos.java
+++ src/main/java/com/ejemplo/SpringBoot/Controller/ControllerProyectos.java
@@ -9,12 +9,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = "https://portfolio-natalia.web.app/")
 public class ControllerProyectos {
     
     @Autowired
@@ -35,5 +36,8 @@ public class ControllerProyectos {
     public void eliminarProyecto (@PathVariable Long id) {
         proyeService.eliminarProyecto(id);
     }
-    
+    @PutMapping ("/edit/proyecto")
+    public void editarProyecto (@RequestBody Proyectos proye) {
+        proyeService.editarProyecto(proye);
+    }
 }
